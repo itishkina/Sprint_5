125df8f726530468da164f156fdafee52b1e2a50
Irina Tishkina <itish77@yandex.ru>
2026-04-29 09:46:31 +0300

    Использовать базовый класс Кошачьи (Feline) для класса Lion

--- src/test/java/LionTest.java
+++ src/test/java/LionTest.java
@@ -1,6 +1,6 @@
 
+import com.example.Feline;
 import com.example.Lion;
-import com.example.Predator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -17,26 +17,26 @@ import static org.mockito.Mockito.*;
 public class LionTest {
 
     @Mock
-    private Predator predator;
+    private Feline feline;
 
 
     @Test
     @DisplayName("hasMane = true, если пол = Самец")
     public void constructorShouldSetHasManeTrueForMale() throws Exception {
-        Lion lion = new Lion("Самец", predator);
+        Lion lion = new Lion("Самец", feline);
         assertTrue(lion.doesHaveMane(), "У самца должна быть грива");
     }
     @Test
     @DisplayName("hasMane = false, если пол = Самка")
     public void constructorShouldSetHasManeFalseForFemale() throws Exception {
-        Lion lion = new Lion("Самка", predator);
+        Lion lion = new Lion("Самка", feline);
         assertFalse(lion.doesHaveMane(), "У самки нет гривы");
     }
     @Test
     @DisplayName("Недопустимое значение пола животного")
     public void constructorShouldThrowExceptionForInvalidSex() {
         Exception exception = assertThrows(Exception.class, () -> {
-            new Lion("Неизвестный", predator);
+            new Lion("Неизвестный", feline);
         });
         assertEquals("Используйте допустимые значения пола животного - самец или самка",
                 exception.getMessage());
@@ -45,8 +45,8 @@ public class LionTest {
     @Test
     @DisplayName("getKittens() возвращает 1 животное")
     void getKittensShouldReturnDefaultValue() throws Exception {
-        when(predator.getKittens()).thenReturn(1);
-        Lion lion = new Lion("Самец", predator);
+        when(feline.getKittens()).thenReturn(1);
+        Lion lion = new Lion("Самец", feline);
         assertEquals(1, lion.getKittens(), "Количество животных должно быть 1");
     }
 
@@ -54,9 +54,9 @@ public class LionTest {
     @DisplayName("getFood() возвращает еду хищника")
     void getFoodShouldReturnCustomFoodFromPredator() throws Exception {
         List<String> expectedFood = List.of("Мясо", "Птица", "Рыба");
-        when(predator.eatMeat()).thenReturn(expectedFood);
+        when(feline.eatMeat()).thenReturn(expectedFood);
 
-        Lion lion = new Lion("Самка", predator);
+        Lion lion = new Lion("Самка", feline);
         List<String> food = lion.getFood();
 
         assertEquals(expectedFood, food, "Список еды должен включать мясо, птицу и рыбу");
